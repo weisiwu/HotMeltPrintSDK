a0bfa07f0b68bb956975f73edc1394cfdf5edae9
weisiwu <2436887475@qq.com>
2025-01-02 22:19:58 +0800

    feat: 打jar包

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,5 @@
 plugins {
-    id("java-library")
-    id("com.github.johnrengelman.shadow") version "6.1.0"
-    kotlin("jvm") version "1.5.30"  // 直接声明插件和版本
+    kotlin("jvm") version "1.5.30"
 }
 
 java {
@@ -15,43 +13,19 @@ tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     }
 }
 
-buildscript {
-    repositories {
-        maven {
-            url = uri("https://maven.aliyun.com/repository/google")
-        }
-        maven {
-            url = uri("https://maven.aliyun.com/repository/public")
-        }
-        maven {
-            url = uri("https://maven.aliyun.com/repository/jcenter")
-        }
-        mavenCentral() // 使用 Maven 中央仓库
-        google() // 使用 Google 的 Maven 仓库
-        jcenter()
-    }
-
-    dependencies {
-        classpath("com.android.tools.build:gradle:4.1.3")  // 如果是 Android 项目，配置 Android Gradle Plugin
-//        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.30")
-    }
+repositories {
+    mavenCentral()
 }
 
 dependencies {
-    implementation(fileTree(mapOf("include" to listOf("*.jar"), "dir" to "libs")))
-    implementation(files("libs/SDKLib.jar"))
-    implementation(files("libs/dvlib-27.1.3.jar"))
-    implementation(files("libs/asm-tree-7.0.jar"))
-    implementation(files("libs/lint-model-27.1.3.jar"))
-    implementation(files("libs/sdk-common-27.1.3.jar"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-common:1.5.1")
     implementation("org.apache.commons:commons-math3:3.6.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3") // 替换为最新版本
+    implementation(fileTree(mapOf("include" to listOf("*.jar"), "dir" to "libs")))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
 }
 
-tasks {
-    shadowJar {
-        archiveBaseName.set("example-fat-jar")
-        archiveVersion.set("1.0")
-        archiveClassifier.set("") // 确保没有附加分类后缀
-    }
+task<Jar>("buildJar") {
+    archiveBaseName.set("my-library")
+    archiveVersion.set("1.0")
+    from(sourceSets.main.get().output)
 }
